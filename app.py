e53ee0ba5d7435a977e9ff1806a522843090b280
Sowmya Kolluru <Kamalasowmya@gmail.com>
2024-04-06 19:30:13 +0530

    /Search

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for, session
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_mysqldb import MySQL
-from flask_mail import Mail
 import MySQLdb.cursors
-import re
 
 app = Flask(__name__)
-#app.secret_key = 'your secret key'
  
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -15,71 +11,55 @@ app.config['MYSQL_DB'] = 'leveluploungedb'
  
 mysql = MySQL(app)
 
-# Login 
+# search content from posts 
 @app.route('/')
-@app.route('/login', methods =['POST']) #or just POST would do
-def login():
-    msg = ''
-    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
-        email = request.form['email']
-        password = request.form['password']
-        print(email, password)
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM userdetails WHERE email = %s AND password = %s', (email, password, ))
-        details = cursor.fetchone()
-        if details:            
-            msg = 'Logged in successfully !'
-            response = {
-                "message" : msg 
-            }
-            return jsonify(response)
-        else:
-            msg = 'Incorrect email / password !'
-    response = {
+@app.route('/search', methods =['GET']) 
+def search():
+    query = request.args.get('query')
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    sqlquery = "SELECT * FROM publications WHERE content LIKE "+ "'%" + query + "%'"
+    print(sqlquery)
+    cursor.execute(sqlquery)
+    details = cursor.fetchall()
+    print(details)
+    if details:            
+        msg = 'Found posts with this keyword !'
+        response = {
+            "message"  : msg, 
+        }
+        return jsonify(details)
+    else:
+        msg = 'No posts found on this!'
+    details  = {
         "message" : msg 
     }
-    return jsonify(response)
+    return jsonify(details)
 
-#Registration
-@app.route('/register', methods =['POST'])
-def register():
-    msg = ''
-    success = False
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-        print(username, password, email)
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM userdetails WHERE username = %s', (username, ))
-        account = cursor.fetchone()
-        if account:
-            msg = 'Account with specified details exists already!'
-
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must be alphanumeric only!'
-
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email id!'
-
-        elif not username or not password or not email:
-            msg = 'Please fill all the details in form'
-
-        else:
-            cursor.execute('INSERT INTO userdetails VALUES ( %s, %s, %s)', (username, email, password ))
-            mysql.connection.commit()
-            success = True
-
-    elif request.method == 'POST':
-        msg = 'Please fill out the form !'
-        
-    response = {
-        "Error" : msg,
-        "success" : success  
+#serach for users
+@app.route('/')
+@app.route('/searchUser', methods =['GET']) 
+def searchUser():
+    user = request.args.get('user')
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    sqlquery = "SELECT username FROM userdetails WHERE username LIKE "+ "'%" + user + "%'"
+    print(sqlquery)
+    cursor.execute(sqlquery)
+    details = cursor.fetchall()
+    print(details)
+    if details:            
+        msg = 'Found posts with this keyword !'
+        response = {
+            "message"  : msg, 
+        }
+        return jsonify(details)
+    else:
+        msg = 'No posts found on this!'
+    details  = {
+        "message" : msg 
     }
-    return jsonify(response)
-
+    return jsonify(details)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
-print("End!")
+
+print("Done!")
